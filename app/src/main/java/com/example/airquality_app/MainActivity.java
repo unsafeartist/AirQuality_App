1fd0ed534c9e75bf851c08bf9fdde9b318d9e130
Kamal <kamaljot.saini97@gmail.com>
2020-08-26 22:24:34 -0700

    Modifying/revising app UI and connected some UI elements to backend Java code

--- app/src/main/java/com/example/airquality_app/MainActivity.java
+++ app/src/main/java/com/example/airquality_app/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.airquality_app;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView location_textView, altitude_textView, temperature_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign the locally declared text-views to the ones in the XML design
+        location_textView = (TextView)findViewById(R.id.location_xml_textView);
+        altitude_textView = (TextView)findViewById(R.id.altitude_xml_textVIew);
+        temperature_textView = (TextView)findViewById(R.id.temperature_xml_textView);
+
     }
+
 }
